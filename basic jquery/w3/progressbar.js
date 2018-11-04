cee3fc259c3770996826d64d0dff4771945061eb
simongarisch <gatman946@gmail.com>
2018-11-04 12:44:21 +1100

    Adding a custom progressbar through jquery ui

---
+++ basic jquery/w3/progressbar.js
@@ -0,0 +1,40 @@
+// https://learn.jquery.com/jquery-ui/widget-factory/how-to-use-the-widget-factory/
+
+$.widget( "custom.progressbar", {
+
+    options: {
+        value: 0
+    },
+
+    _create: function() {
+        var progress = this.options.value + "%";
+        this.element
+            .addClass( "progressbar" )
+            .text( progress );
+    },
+
+    // Create a public method.
+    value: function( value ) {
+
+        // No value passed, act as a getter.
+        if ( value === undefined ) {
+            return this.options.value;
+        }
+
+        // Value passed, act as a setter.
+        this.options.value = this._constrain( value );
+        var progress = this.options.value + "%";
+        this.element.text( progress );
+    },
+
+    // Create a private method.
+    _constrain: function( value ) {
+        if ( value > 100 ) {
+            value = 100;
+        }
+        if ( value < 0 ) {
+            value = 0;
+        }
+        return value;
+    }
+});
